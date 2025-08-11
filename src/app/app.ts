bce65f9dd372025177752ef15ae3f3345c289abf
mert.celik <mert.celik@caretta.net>
2025-08-11 19:30:55 +0300

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+import { SwiperOptions } from 'swiper/types';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +15,17 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('watches-cost-finder');
+
+  ngOnInit() {
+    new Swiper('.swiper', {
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  }
 }
