746e4e13fb0483e39e5443ceb89cb64d29ff3d35
mert.celik <mert.celik@caretta.net>
2025-08-11 22:30:34 +0300

    update: swiper bug fix

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,7 @@
 import { isPlatformBrowser } from '@angular/common';
 import { Component, Inject, PLATFORM_ID, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-// import Swiper bundle with all modules installed
 import Swiper from 'swiper/bundle';
-
-// import styles bundle
 import 'swiper/css/bundle';
 import { SwiperOptions } from 'swiper/types';
 
@@ -12,13 +9,15 @@ import { SwiperOptions } from 'swiper/types';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('watches-cost-finder');
 
+  constructor() {}
+
   ngAfterViewInit() {
-    new Swiper('.container .swiper-container .swiper', {
+    const swiper = new Swiper('.swiper', {
       loop: true,
       pagination: {
         el: '.swiper-pagination',
