d4ca9d285034178d69b4f7ff26430a83a0032cad
mert.celik <mert.celik@caretta.net>
2025-08-12 17:36:12 +0300

    update: emailjs integrated

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,36 @@
-import { isPlatformBrowser } from '@angular/common';
-import { Component, Inject, PLATFORM_ID, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
-import { SwiperOptions } from 'swiper/types';
-
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('watches-cost-finder');
 
-  constructor() {}
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      fullName: [''],
+      phone: [''],
+      email: [''],
+      brand: [''],
+      model: [''],
+      year: [null],
+      condition: [''],
+      box: [false],
+      papers: [false],
+      limitedEdition: [false],
+      serviceHistory: [false],
+      file: [null],
+    });
+  }
 
   ngAfterViewInit() {
     const swiper = new Swiper('.swiper', {
@@ -31,5 +47,31 @@ export class App {
         prevEl: '.swiper-button-prev',
       },
     });
+  }
+
+  onSubmit(event: Event) {
+    if (this.form.valid) {
+      emailjs
+        .send(
+          'service_hpzckig',
+          'template_z1nkavg',
+          {
+            ...this.form.value,
+          },
+          {
+            publicKey: 'ff99_4o7G_G1qNZga',
+          }
+        )
+        .then(
+          () => {
+            console.log('SUCCESS!');
+          },
+          (error) => {
+            console.log('FAILED...', error as EmailJSResponseStatus);
+          }
+        );
+      alert('Form başarıyla gönderildi!');
+      this.form.reset();
+    }
   }
 }
